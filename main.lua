f3c4335a84372ee063cb6e26180a0a65ce54897e
Relton <ori.taylor@gmail.com>
2019-07-19 13:30:53 +0100

    Fixing merge issue

--- main.lua
+++ main.lua
@@ -16,9 +16,8 @@ function love.load()
 	audio.load()
 	input.load()
 	ui.load()
-	physics.load()
-
 	ui.initForGame()
+	physics.load()
 	game.load()
 
 end
@@ -31,9 +30,8 @@ function love.update(dt)
 	input.update()
 	audio.update()
 	ui.update()
-	physics.update(dt)
-
 	ui.inGameMenu("escape",{"gameMenu1","gameMenu2","gameMenu3"})
+	physics.update(dt)
 	game.update(dt)
 
 end
@@ -41,20 +39,7 @@ end
 function love.draw()
 
 	love.graphics.scale(scale_X,scale_Y)
-
 	ui.draw()
 	game.draw()
-
-	-------------
-
-	love.graphics.setColor(0.28, 0.63, 0.05) -- set the drawing color to green for the ground
-  	love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape:getPoints())) -- draw a "filled in" polygon using the ground's coordinates
- 
-  	love.graphics.setColor(0.76, 0.18, 0.05) --set the drawing color to red for the ball
-  	love.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
- 
-  	love.graphics.setColor(0.20, 0.20, 0.20) -- set the drawing color to grey for the blocks
-  	love.graphics.polygon("fill", objects.block1.body:getWorldPoints(objects.block1.shape:getPoints()))
-  	love.graphics.polygon("fill", objects.block2.body:getWorldPoints(objects.block2.shape:getPoints()))
 
 end

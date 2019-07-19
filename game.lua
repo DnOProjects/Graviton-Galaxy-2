c5c1196cdcf705f4e2f7e8410c1e1fac547a273b
Danny <danzo2303@gmail.com>
2019-07-19 15:46:44 +0100

    Iterative block adding

--- game.lua
+++ game.lua
@@ -1,15 +1,14 @@
+local Vector = require "vector"
+local physics = require "physics"
+
 local game = {}
 
 function game.load()
 
-	love.physics.setMeter(64) --the height of a meter our worlds will be 64px
-	world = love.physics.newWorld(0, 9.81*64, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
-	objects = {} -- table to hold all our physical objects
-
 	--let's create the ground
 	objects.ground = {}
 	objects.ground.body = love.physics.newBody(world, 650/2, 650-50/2) --remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
-	objects.ground.shape = love.physics.newRectangleShape(650, 50) --make a rectangle with a width of 650 and a height of 50
+	objects.ground.shape = love.physics.newRectangleShape(2000, 50) --make a rectangle with a width of 650 and a height of 50
 	objects.ground.fixture = love.physics.newFixture(objects.ground.body, objects.ground.shape); --attach shape to body
 
 	--let's create a ball
@@ -20,16 +19,9 @@ function game.load()
 	objects.ball.fixture:setRestitution(0.9) --let the ball bounce
 
 	--let's create a couple blocks to play around with
-	objects.block1 = {}
-	objects.block1.body = love.physics.newBody(world, 200, 550, "dynamic")
-	objects.block1.shape = love.physics.newRectangleShape(0, 0, 50, 100)
-	objects.block1.fixture = love.physics.newFixture(objects.block1.body, objects.block1.shape, 5) -- A higher density gives it more mass.
-
-	objects.block2 = {}
-	objects.block2.body = love.physics.newBody(world, 200, 400, "dynamic")
-	objects.block2.shape = love.physics.newRectangleShape(0, 0, 100, 50)
-	objects.block2.fixture = love.physics.newFixture(objects.block2.body, objects.block2.shape, 2)
-
+	for i=1,20 do
+		physics.addRectangularObject({position=Vector(i*50,550),size=Vector(20,100),density=0.5,bodyType="dynamic"})
+	end
 
 end
 
@@ -48,8 +40,11 @@ function game.draw()
 		love.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
 
 		love.graphics.setColor(0.20, 0.20, 0.20) -- set the drawing color to grey for the blocks
-		love.graphics.polygon("fill", objects.block1.body:getWorldPoints(objects.block1.shape:getPoints()))
-		love.graphics.polygon("fill", objects.block2.body:getWorldPoints(objects.block2.shape:getPoints()))
+		for i=1,#objects do
+			local object = objects[i]
+			love.graphics.polygon("fill", object.body:getWorldPoints(object.shape:getPoints()))
+		end
+
 	else
 		love.graphics.setBackgroundColor(0, 0, 0)
 	end

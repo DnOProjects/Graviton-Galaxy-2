caab43234df4cecb6bf6e2c6bf880064967675a3
Danny <danzo2303@gmail.com>
2019-07-21 18:02:02 +0100

    Fixed flickering bug

--- images.lua
+++ images.lua
@@ -2,6 +2,8 @@ local images = {}
 
 function images.load()
 
+	love.graphics.setDefaultFilter("nearest","nearest",0)
+
 	images.dirt = love.graphics.newImage("Images/dirt.png")
 	images.block = love.graphics.newImage("Images/block.png")
 	images.planet = love.graphics.newImage("Images/planet.png")
